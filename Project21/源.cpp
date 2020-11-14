b2874a218d7c4cab5594faa704e38bae3ee6daf9
liquor <1956810010@qq.com>
2020-11-14 16:15:57 +0800

    对于软件基础的更新

--- Project21/源.cpp
+++ Project21/源.cpp
@@ -1,21 +1,23 @@
 #include <stdlib.h>
 #include"stdio.h"
+#include"math.h"
 
-#define	maxsize	1024
+#define    maxsize    1024
 typedef int datatype;
+char e[maxsize];
 
 typedef struct
 {
-    datatype elements[maxsize];
+    datatype data[maxsize];
     int Top;
 }Stack;
 
-void setNull(Stack *S)
+void Init_Stack(Stack* S)
 {
     S->Top = -1;
 }
 
-int isfull(Stack *S)
+int isfull(Stack* S)
 {
     if (S->Top == maxsize - 1)
         return (1);
@@ -23,7 +25,7 @@ int isfull(Stack *S)
         return (0);
 }
 
-int isempty(Stack *S)
+int isempty(Stack* S)
 {
     if (S->Top >= 0)
         return (0);
@@ -31,70 +33,100 @@ int isempty(Stack *S)
         return (1);
 }
 
-void push(Stack *S, datatype E)
+void push(Stack* S, datatype x)
 {
     if (S->Top == maxsize - 1)
     {
-        printf("Stack Overflow");/*上溢现象*/
+        printf("堆栈已满\n");
     }
     else
     {
         S->Top++;
-        S->elements[S->Top] = E;
+        S->data[S->Top] = x;
     }
 
 }
 
-datatype *pop(Stack *S)
+datatype pop(Stack* S)
 {
-    datatype *temp;
+    datatype temp;
     if (isempty(S))
     {
-        printf("Stack Underflow");
+        printf("堆栈为空\n");
         return (NULL);
     }
     else
     {
         S->Top--;
-        temp = (datatype*)malloc(sizeof(datatype));
-        *temp = S->elements[S->Top+1];
+        temp = S->data[S->Top + 1];
     }
     return (temp);
 }
 
-void conversion(int n)
+int  zhuanhuan(int num, int n)
+{
+    Stack K;
+    Init_Stack(&K);
+    while (num)
+    {
+
+        push(&K, num % 10);
+        num = num / 10;
+    }
+
+    while (!isempty(&K))
+    {
+        num = pow((float)n, K.Top) * pop(&K) + num;
+    }
+    printf("十进制数%d", num);
+    return num;
+}
+
+void conversion(int n, int B)
 {
     Stack S;
-    setNull(&S);
+    Init_Stack(&S);
     int r, m;
+    char i;
     r = n;
     while (r)
     {
-        m = r % 2;
+        m = r % B;
         if (isfull(&S))
-            printf("Over flow\n");
+            printf("数据过大\n");
         else
         {
-            push(&S,m);
+            push(&S, e[m]);
         }
 
-        r = r / 2;
+        r = r / B;
     }
-    printf("\n转换后的二进制为\n");
+    printf("\n转换后的数为");
     while (!isempty(&S))
     {
-        printf("%d", *(pop(&S)));
+        i = pop(&S);
+        if (i >= 65 && i <= 122)
+            printf("%c", i);
+        else if (i >= 0 && i < 10)
+            printf("%d", i);
+        printf("\n");
     }
     printf("\n\n");
 }
 
 int main()
 {
-    int num;
+    int num, m, n;
+    for (int i = 0; i < 10; i++)
+        e[i] = i;
+    for (int i = 10; i < 20; i++)
+        e[i] = i + 55;
     while (1)
     {
-        printf("请输入需要转换为二进制的十进制数据\n");
-        scanf_s("%d", &num);
-        conversion(num);
+        printf("请输入需要转换为其他进制的任意进制数据\n");
+        scanf_s("%d %d %d", &num, &m, &n);
+        if (num == 0) break;
+        num = zhuanhuan(num, m);
+        conversion(num, n);
     }
 }
